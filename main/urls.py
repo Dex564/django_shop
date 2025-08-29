5669c796fbd2f434217b80c672929753a4d5f1b0
Dex564 <orlovpetr2007@gmail.com>
2025-08-29 10:57:01 +0400

    made views and urls

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls  import path
+from . import views
+
+app_name = 'main' # нужен для генерации нейм спейсов, чтобы мы могли обращаться к разным функциям и там ещё чета с шаблонами
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('catalog/', views.CatalogView.as_view(), name='catalog_all'),
+    path('catlog/<slug:category_slug>/', views.CatalogView.as_view(), name='catalog'),
+    path('product<slug:slug>', views.ProductDetailView.as_view(), name='product_detail'),
+]
+
+
